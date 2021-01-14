12c42198a1359de3085785efe30416057205d8f4
Michael <mcjoy465@gmail.com>
2021-01-13 20:27:20 -0500

    dev

--- config.js
+++ config.js
@@ -1,18 +1,18 @@
 exports.project_q_dev = {
-	region: "",
-	pathToFolder: "",
-	s3Bucket: "",
-	lambdaFunction: "",
+    region: "us-east-2",
+    pathToFolder: "../../ProjectQ/Services/develop/projectq-services",
+    s3Bucket: "ohio-lambda-functions",
+    lambdaFunction: "ProjectQ_Services_Dev",
 }
 exports.ar_prod = {
-	region: "",
-	pathToFolder: "",
-	s3Bucket: "",
-	lambdaFunction: "",
+    region: "us-east-2",
+    pathToFolder: "../../Autorest/Services/autorest-services/updatePaymentMethod",
+    s3Bucket: "autorest-ohio-lambdas",
+    lambdaFunction: "updatePaymentMethod",
 }
 exports.ar_dev = {
-	region: "",
-	pathToFolder: "",
-	s3Bucket: "",
-	lambdaFunction: "",
+    region: "us-east-2",
+    pathToFolder: "../../Autorest/Services/autorest-services/successfulPaymentsStripeWebhook",
+    s3Bucket: "autorest-ohio-lambdas-dev",
+    lambdaFunction: "successfulPaymentsStripeWebhook",
 }
